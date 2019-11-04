307d87f1c01add5e1016ee578a8dd886394beab2
Kevin Allen <kevin.allen91@gmail.com>
2019-11-04 13:04:16 -0800

    mvp complete

---
+++ budgetRouter.js
@@ -0,0 +1,42 @@
+const express = require('express')
+
+const db = require('./data/dbConfig')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db('accounts')
+        .then(account => res.status(200).json(account))
+        .catch(err => res.status(500).json({error: 'Error getting data'}))
+})
+
+router.get('/:id', (req, res) => {
+    db('accounts')
+        .where({id: req.params.id})
+            .then(account => res.status(200).json(account))
+            .catch(err => res.status(500).json({error: 'error getting account'}))
+})
+
+router.post('/', (req, res) => {
+    db  
+        .insert(req.body, 'id')
+        .into('accounts')
+        .then(account => res.status(201).json(account))
+        .catch(err => res.status(500).json({error: 'Error posting account'}))
+})
+
+router.put('/:id', (req, res) => {
+    db('accounts')
+        .where({id: req.params.id})
+        .update(req.body)
+        .then(account => res.status(201).json(account))
+        .catch(err => res.status(500).json({error: 'Error updating account'}))  
+
+})
+
+router.delete('/:id', (req, res) => {
+    db('accounts')
+        .where({id: req.params.id})
+        .del()
+        .then(account => res.status(200).json(account))
+        .catch(err => res.status(500).json({error: 'Error deleting the account'}))
+})
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const budgetRouter = require('./budgetRouter')
 
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/budget', budgetRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Budgets DB"})
+})
 
 module.exports = server;
